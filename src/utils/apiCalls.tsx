d36606a3afb9603395a7673f9116a70e6067d10e
Nathan Darrington <npdarrington@icloud.com>
2020-11-08 19:16:06 -0700

    Created getQuizQuestions async function and modified imports to ensure correct data type is being matched

--- src/utils/apiCalls.tsx
+++ src/utils/apiCalls.tsx
@@ -1,1 +1,31 @@
-import { Difficulty, QuizQuestionModel, randomizeAnswers } from './utils';
+import {
+	Difficulty,
+	QuestionAPIModel,
+	QuizQuestionModel,
+	randomizeAnswers,
+} from './utils';
+
+const quizQuestionsUrl = `https://opentdb.com/api.php`;
+
+export const getQuizQuestions = async (
+	amount: number,
+	difficulty: Difficulty
+): Promise<QuizQuestionModel | string> => {
+	const fetchQuestions = await fetch(
+		`${quizQuestionsUrl}?amount=${amount}&difficulty=${difficulty}&type=multiple`
+	);
+	if (fetchQuestions.ok) {
+		const quizData = await fetchQuestions.json();
+		return quizData.results.map((question: QuestionAPIModel) => {
+			return {
+				...question,
+				answers: randomizeAnswers([
+					...question.incorrect_answers,
+					question.correct_answer,
+				]),
+			};
+		});
+	} else {
+		return 'error';
+	}
+};
