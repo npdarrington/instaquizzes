5551c218044125bb410af70dbea7494ead7ae165
Nathan Darrington <npdarrington@icloud.com>
2020-11-09 13:23:11 -0700

    Set up import statements for testing Question

---
+++ src/Question/Question.test.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen, render, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
