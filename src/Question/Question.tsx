8b3c55789f9e503d5b0a3bbd25bab3f516efe942
Nathan Darrington <npdarrington@icloud.com>
2020-11-08 20:36:16 -0700

    Destructure category prop and display for user on page

--- src/Question/Question.tsx
+++ src/Question/Question.tsx
@@ -4,10 +4,10 @@ interface IProps {
 	category: string;
 }
 
-const Question: React.FC<IProps> = () => {
+const Question: React.FC<IProps> = ({ category }) => {
 	return (
 		<article className='question-card'>
-			<h3 className='question-number'>Quiz Category Here</h3>
+			<h3 className='question-number'>{category}</h3>
 			<p className='question-number'>Question: 1 / 15</p>
 			<p className='question'>Insert Question Here</p>
 			<section className='answers'>Insert Answers Here</section>
