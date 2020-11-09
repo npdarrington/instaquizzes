d2cc3d92ab1efee7666fb56efce5efab5b8b3946
Nathan Darrington <npdarrington@icloud.com>
2020-11-09 14:58:46 -0700

    Expanded on imports needed for App Integration Testing

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { getQuizQuestions } from '../utils/apiCalls';
+
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
 });
