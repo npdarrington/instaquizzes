4de87d0c743d69f8eacca9df1e3a07ec3da970b3
Nathan Darrington <npdarrington@icloud.com>
2020-11-08 20:06:30 -0700

    Added initial structure for how a question will display

---
+++ src/Question/Question.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Question = () => {
+	return (
+		<article className='question-card'>
+			<p className='question-number'>Question: 1 / 15</p>
+			<p className='question'>Insert Question Here</p>
+			<section className='answers'>Insert Answers Here</section>
+		</article>
+	);
+};
+
+export default Question;
