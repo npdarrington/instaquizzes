9fc629b9c8d660c103a8de292c897e6e4f35d028
Nathan Darrington <npdarrington@icloud.com>
2020-11-09 16:25:47 -0700

    Test should show the next question button as Finish Quiz after answering last question

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -5,6 +5,7 @@ import '@testing-library/jest-dom';
 
 import { getQuizQuestions } from '../utils/apiCalls';
 import { QuizQuestionModel } from '../utils/utils';
+import mockQuizQuestions from './mockQuizQuestions';
 
 import App from './App';
 
@@ -13,28 +14,7 @@ jest.mock('../utils/apiCalls.tsx');
 describe('App', () => {
 	let mockQuestions: QuizQuestionModel[];
 	beforeEach(() => {
-		mockQuestions = [
-			{
-				category: 'Entertainment: Music',
-				type: 'multiple',
-				difficulty: 'easy',
-				question:
-					"In the  Rossini opera, what was the name of 'The Barber of Seville'?",
-				correct_answer: 'Figaro',
-				incorrect_answers: ['Angelo', 'Fernando', 'Dave'],
-				answers: ['Dave', 'Fernando', 'Figaro', 'Angelo'],
-			},
-			{
-				category: 'Entertainment: Video Games',
-				type: 'multiple',
-				difficulty: 'easy',
-				question:
-					'Which of these is NOT a map included in the game Counter-Strike: Global Offensive?',
-				correct_answer: 'Oilrig',
-				incorrect_answers: ['Assault', 'Mirage', 'Cache'],
-				answers: ['Assault', 'Oilrig', 'Mirage', 'Cache'],
-			},
-		];
+		mockQuestions = mockQuizQuestions;
 	});
 
 	test('should render instaquizzes to the screen', () => {
@@ -56,10 +36,10 @@ describe('App', () => {
 			screen.getByRole('button', { name: 'Start a New InstaQuiz' })
 		);
 		const questionCategory = await waitFor(() =>
-			screen.getByText('Entertainment: Music')
+			screen.getByText('Entertainment: Video Games')
 		);
 		const question = screen.getByText(
-			"In the Rossini opera, what was the name of 'The Barber of Seville'?"
+			'Which of these is NOT a player class in Team Fortress 2?'
 		);
 		const answersBtns = screen.getAllByRole('button');
 		const answerBtn1 = answersBtns[1].innerHTML;
@@ -68,10 +48,10 @@ describe('App', () => {
 		const answerBtn4 = answersBtns[4].innerHTML;
 		expect(questionCategory).toBeInTheDocument();
 		expect(question).toBeInTheDocument();
-		expect(answerBtn1).toBe('Dave');
-		expect(answerBtn2).toBe('Fernando');
-		expect(answerBtn3).toBe('Figaro');
-		expect(answerBtn4).toBe('Angelo');
+		expect(answerBtn1).toBe('Spy');
+		expect(answerBtn2).toBe('Demoman');
+		expect(answerBtn3).toBe('Pyro');
+		expect(answerBtn4).toBe('Healer');
 	});
 
 	test('should allow a user to answer a question when clicking an answer button', async () => {
@@ -80,7 +60,7 @@ describe('App', () => {
 		userEvent.click(
 			screen.getByRole('button', { name: 'Start a New InstaQuiz' })
 		);
-		await waitFor(() => screen.getByText('Entertainment: Music'));
+		await waitFor(() => screen.getByText('Entertainment: Video Games'));
 		const answersBtns = screen.getAllByRole('button');
 		const answerBtn3 = answersBtns[3];
 		userEvent.click(answerBtn3);
@@ -95,17 +75,15 @@ describe('App', () => {
 		userEvent.click(
 			screen.getByRole('button', { name: 'Start a New InstaQuiz' })
 		);
-		await waitFor(() => screen.getByText('Entertainment: Music'));
+		await waitFor(() => screen.getByText('Entertainment: Video Games'));
 		const answerBtnFirstQuestion = screen.getByRole('button', {
-			name: 'Figaro',
+			name: 'Demoman',
 		});
 		userEvent.click(answerBtnFirstQuestion);
 		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
-		const questionCategory = await waitFor(() =>
-			screen.getByText('Entertainment: Video Games')
-		);
+		const questionCategory = screen.getByText('Entertainment: Video Games');
 		const question = screen.getByText(
-			'Which of these is NOT a map included in the game Counter-Strike: Global Offensive?'
+			'Which of these levels does NOT appear in the console/PC versions of the game "Sonic Generations"?'
 		);
 		const answersBtns = screen.getAllByRole('button');
 		const answerBtn1 = answersBtns[1].innerHTML;
@@ -114,9 +92,109 @@ describe('App', () => {
 		const answerBtn4 = answersBtns[4].innerHTML;
 		expect(questionCategory).toBeInTheDocument();
 		expect(question).toBeInTheDocument();
-		expect(answerBtn1).toBe('Assault');
-		expect(answerBtn2).toBe('Oilrig');
-		expect(answerBtn3).toBe('Mirage');
-		expect(answerBtn4).toBe('Cache');
+		expect(answerBtn1).toBe('City Escape');
+		expect(answerBtn2).toBe('Mushroom Hill');
+		expect(answerBtn3).toBe('Planet Wisp');
+		expect(answerBtn4).toBe('Sky Sanctuary');
+	});
+
+	test('should show the next question button as Finish Quiz after answering last question', async () => {
+		(getQuizQuestions as jest.Mock).mockResolvedValue(mockQuestions);
+		render(<App />);
+		userEvent.click(
+			screen.getByRole('button', { name: 'Start a New InstaQuiz' })
+		);
+		await waitFor(() => screen.getByText('Entertainment: Video Games'));
+		const answerBtnFirstQuestion = screen.getByRole('button', {
+			name: 'Demoman',
+		});
+		userEvent.click(answerBtnFirstQuestion);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Sky Sanctuary',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: '7',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Hungarian',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Mercy',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Sprinter',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Benjamin Franklin',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Steve Blum',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Toad',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Japan',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: '402',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Rome',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Arceus',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Pudding Lane',
+			})
+		);
+		userEvent.click(screen.getByRole('button', { name: 'Next Question' }));
+		userEvent.click(
+			screen.getByRole('button', {
+				name: 'Kyoko Ito',
+			})
+		);
+		expect(
+			screen.getByRole('button', { name: 'Finish Quiz' })
+		).toBeInTheDocument();
 	});
 });
