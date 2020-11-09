0a5174e8b158ba9755833aa591bb41a4bfbfe701
Nathan Darrington <npdarrington@icloud.com>
2020-11-08 20:13:27 -0700

    Added category title inside question-card

--- src/Question/Question.tsx
+++ src/Question/Question.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Question = () => {
 	return (
 		<article className='question-card'>
+			<h3 className='question-number'>Quiz Category Here</h3>
 			<p className='question-number'>Question: 1 / 15</p>
 			<p className='question'>Insert Question Here</p>
 			<section className='answers'>Insert Answers Here</section>
