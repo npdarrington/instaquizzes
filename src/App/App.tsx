8daa758e6f6561f039dce7d364859e6aac0fc0d9
Nathan Darrington <npdarrington@icloud.com>
2020-11-08 20:28:26 -0700

    Imports to get quiz questions from api, type check them and test API call to store in state

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
 
-import { QuizQuestionModel } from '../utils/utils';
+import { QuizQuestionModel, Difficulty } from '../utils/utils';
+import { getQuizQuestions } from '../utils/apiCalls';
 
 import './App.scss';
 
 function App() {
 	const [questions, setQuestions] = useState<QuizQuestionModel[]>([]);
 
+	const startQuiz = async () => {
+		try {
+			const newQuizQuestions = await getQuizQuestions(15, Difficulty.EASY);
+			setQuestions(newQuizQuestions);
+		} catch (_err) {
+			console.log(_err);
+		}
+	};
+
 	return (
 		<div className='App'>
 			<h1>InstaQuizzes!</h1>
+			<button onClick={startQuiz}>Click me for testing!</button>
 		</div>
 	);
 }
