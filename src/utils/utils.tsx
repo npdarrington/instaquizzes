3f1d0a1501d6f0003cfb99df857fe3e398b7c820
Nathan Darrington <npdarrington@icloud.com>
2020-11-08 18:51:13 -0700

    Added Difficulty enum

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,3 +1,9 @@
-export const randomizeAnswer = (answers: string[]) => {
-  Math.floor(Math.random() * answers.length);
+export enum Difficulty {
+	EASY = 'easy',
+	MEDIUM = 'medium',
+	HARD = 'hard',
 }
+
+export const randomizeAnswer = (answers: string[]) => {
+	Math.floor(Math.random() * answers.length);
+};
