0f0297b33ee7557efac81c260b13d0abf0a1cf2a
Nathan Darrington <npdarrington@icloud.com>
2020-11-08 20:44:34 -0700

    Added question type check to props, destructed from props and display to user

--- src/Question/Question.tsx
+++ src/Question/Question.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 interface IProps {
 	category: string;
+	question: string;
 }
 
-const Question: React.FC<IProps> = ({ category }) => {
+const Question: React.FC<IProps> = ({ category, question }) => {
 	return (
 		<article className='question-card'>
 			<h3 className='question-number'>{category}</h3>
 			<p className='question-number'>Question: 1 / 15</p>
-			<p className='question'>Insert Question Here</p>
+			<p className='question'>{question}</p>
 			<section className='answers'>Insert Answers Here</section>
 		</article>
 	);
