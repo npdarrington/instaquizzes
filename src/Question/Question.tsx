8162a6a25a9920feea4861d999342a069fa9b5b5
Nathan Darrington <npdarrington@icloud.com>
2020-11-08 21:34:39 -0700

    Used dangerouslySetInnerHTML to render raw HTML

--- src/Question/Question.tsx
+++ src/Question/Question.tsx
@@ -10,7 +10,7 @@ const Question: React.FC<IProps> = ({ category, question }) => {
 		<article className='question-card'>
 			<h3 className='question-number'>{category}</h3>
 			<p className='question-number'>Question: 1 / 15</p>
-			<p className='question'>{question}</p>
+			<p dangerouslySetInnerHTML={{ __html: question }} />
 			<section className='answers'>Insert Answers Here</section>
 		</article>
 	);
