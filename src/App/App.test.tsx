a9b14465be363bf902c0dda02f4e7695cbea638c
Nathan Darrington <npdarrington@icloud.com>
2020-11-09 15:01:57 -0700

    Added import for type QuizQuestionModel and set up mockQuestions for async testing

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -4,11 +4,37 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import { getQuizQuestions } from '../utils/apiCalls';
+import { QuizQuestionModel } from '../utils/utils';
 
 import App from './App';
 
+jest.mock('../utils/utils.tsx');
+
 describe('App', () => {
+	let mockQuestions: QuizQuestionModel[];
 	beforeEach(() => {
+		mockQuestions = [
+			{
+				category: 'Entertainment: Music',
+				type: 'multiple',
+				difficulty: 'easy',
+				question:
+					'In the  Rossini opera, what was the name of &#039;The Barber of Seville&#039;?',
+				correct_answer: 'Figaro',
+				incorrect_answers: ['Angelo', 'Fernando', 'Dave'],
+				answers: ['Dave', 'Fernando', 'Figaro', 'Angelo'],
+			},
+			{
+				category: 'Entertainment: Video Games',
+				type: 'multiple',
+				difficulty: 'easy',
+				question:
+					'Which of these is NOT a map included in the game Counter-Strike: Global Offensive?',
+				correct_answer: 'Oilrig',
+				incorrect_answers: ['Assault', 'Mirage', 'Cache'],
+				answers: ['Assault', 'Oilrig', 'Mirage', 'Cache'],
+			},
+		];
 		render(<App />);
 	});
 });
