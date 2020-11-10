b3c565e67ab400fd61a752ca264b98f359dc9097
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 15:39:06 -0700

    Created displayQuizQuestions method to display each question that is found

--- src/SavedQuizDetails/SavedQuizDetails.tsx
+++ src/SavedQuizDetails/SavedQuizDetails.tsx
@@ -9,12 +9,26 @@ interface IProps {
 }
 
 const SavedQuizDetails: React.FC<IProps> = ({ savedQuiz }) => {
+	const displayQuizQuestions = (quiz: any, index: number) => {
+		return (
+			<section className='saved-quiz-category'>
+				<h3>Category: {quiz.category}</h3>
+				<h3 dangerouslySetInnerHTML={{ __html: quiz.question }} />
+				<h3>Available Answers: </h3>
+				{quiz.answers.map((answer: any) => (
+					<h4>{answer}</h4>
+				))}
+				<h3>Correct Answer: {quiz.correct_answer}</h3>
+			</section>
+		);
+	};
 	return (
 		<section className='saved-quiz'>
 			{savedQuiz.allQuestions.map((quiz, i) => {
 				return (
 					<article key={i} className='saved-quiz-card'>
 						<h3>Question: {i + 1}</h3>
+						{displayQuizQuestions(quiz, i)}
 					</article>
 				);
 			})}
