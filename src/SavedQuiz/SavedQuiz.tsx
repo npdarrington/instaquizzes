c225581683e4b40db14face911d94dbdc84ec559
Nathan Darrington <npdarrington@icloud.com>
2020-11-09 23:46:52 -0700

    Added SavedQuizGamesModel and scss imports

--- src/SavedQuiz/SavedQuiz.tsx
+++ src/SavedQuiz/SavedQuiz.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { SavedQuizGamesModel } from '../utils/utils';
+
+import './SavedQuiz.scss';
+
 const SavedQuiz = () => {
 	return <h1>Saved Quiz Component</h1>;
 };
