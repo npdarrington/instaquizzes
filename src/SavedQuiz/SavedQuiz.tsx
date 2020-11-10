391c70e1051c90cf9e09ae7068a32432379ae511
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 00:02:31 -0700

    Created correctAnswers and display correctAnswers count for user

--- src/SavedQuiz/SavedQuiz.tsx
+++ src/SavedQuiz/SavedQuiz.tsx
@@ -9,12 +9,16 @@ interface IProps {
 }
 
 const SavedQuiz: React.FC<IProps> = ({ savedQuiz }) => {
+	const correctAnswers = (): number => {
+		return savedQuiz.allAnswers.filter(question => question.correct).length;
+	};
 	return (
 		<article className='saved-quiz-card'>
 			<p className='category'>Category: All Random</p>
 			<p className='question-count'>
 				Question Count: {savedQuiz.allQuestions.length}
 			</p>
+			<p className='correct-answers'>Correct Answers: {correctAnswers()}</p>
 		</article>
 	);
 };
