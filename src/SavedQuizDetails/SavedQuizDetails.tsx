1dd38a4e84e1189bec315b428ece1b30e72859f6
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 14:35:28 -0700

    Imported SavedQuizGamesModel and rendered props id on screen

--- src/SavedQuizDetails/SavedQuizDetails.tsx
+++ src/SavedQuizDetails/SavedQuizDetails.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { SavedQuizGamesModel } from '../utils/utils'
+
 import './SavedQuizDetails.scss';
 
-const SavedQuizDetails = () => {
-	return <h1>SavedQuizDetails Component</h1>;
+interface IProps {
+	savedQuiz: SavedQuizGamesModel
+}
+
+const SavedQuizDetails: React.FC<IProps> = ({ savedQuiz }) => {
+	return <h1>SavedQuizDetails Component {savedQuiz.id}</h1>;
 };
 
 export default SavedQuizDetails;
