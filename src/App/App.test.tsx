0b12773c63d5791a3ca030dd85a2eadbb38b5e8b
Nathan Darrington <npdarrington@icloud.com>
2020-11-09 17:40:47 -0700

    Remove act import

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { render, screen, waitFor } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
 import '@testing-library/jest-dom';
 
 import { getQuizQuestions } from '../utils/apiCalls';
