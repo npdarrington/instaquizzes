4a86764aa0d3ca3e6a56ea3e3ef5d763865d51fd
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 08:27:17 -0700

    Test should display SavedQuizzes on screen

--- src/SavedQuizzes/SavedQuizzes.test.tsx
+++ src/SavedQuizzes/SavedQuizzes.test.tsx
@@ -3,3 +3,20 @@ import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import SavedQuizzes from './SavedQuizzes';
+
+import mockSavedQuizzesData from './mockSavedQuizzesData';
+
+describe('SavedQuizzes', () => {
+	beforeEach(() => {
+		render(<SavedQuizzes savedGames={mockSavedQuizzesData} />);
+	});
+
+	test('should display SavedQuizzes on the screen', () => {
+		expect(screen.getByText('Saved Quiz 1')).toBeInTheDocument();
+		expect(screen.getByText('Correct Answers: 1')).toBeInTheDocument();
+		expect(screen.getByText('Incorrect Answers: 1')).toBeInTheDocument();
+		expect(screen.getByText('Saved Quiz 2')).toBeInTheDocument();
+		expect(screen.getByText('Correct Answers: 2')).toBeInTheDocument();
+		expect(screen.getByText('Incorrect Answers: 0')).toBeInTheDocument();
+	});
+});
