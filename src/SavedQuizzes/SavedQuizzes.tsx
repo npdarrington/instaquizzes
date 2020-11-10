95ecc26764ba59d6f0ad4ab645faa65bd63080b7
Nathan Darrington <npdarrington@icloud.com>
2020-11-09 23:10:29 -0700

    Modified return to a saved-games section that will show saved games or conditionally render a message to show play a game and save it

--- src/SavedQuizzes/SavedQuizzes.tsx
+++ src/SavedQuizzes/SavedQuizzes.tsx
@@ -10,7 +10,18 @@ interface IProps {
 
 const SavedQuizzes: React.FC<IProps> = ({ savedGames }) => {
 	return (
-		
+		<section className='saved-games'>
+			{savedGames.length < 1 && (
+				<h2>
+					Play through a new InstaQuiz game and save it to be able to view it
+					here
+				</h2>
+			)}
+			{savedGames.length > 0 &&
+				savedGames.map(savedGame => {
+					return <h1>Saved Game Here</h1>;
+				})}
+		</section>
 	);
 };
 
