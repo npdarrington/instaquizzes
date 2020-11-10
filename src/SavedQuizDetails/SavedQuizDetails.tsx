0b4231388836cf3919efc9d8ebf31ab54ead4f20
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 08:54:24 -0700

    Setup FC boilerplate for SavedQuizDetails

---
+++ src/SavedQuizDetails/SavedQuizDetails.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './SavedQuizDetails.scss';
+
+const SavedQuizDetails = () => {
+	return <h1>SavedQuizDetails Component</h1>;
+};
+
+export default SavedQuizDetails;
