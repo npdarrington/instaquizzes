1e56b31f1d49c6ebef833d1efc2b410923f36ed0
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 18:36:44 -0700

    Test should render all quiz information that is saved in full detail

--- src/SavedQuizDetails/SavedQuizDetails.test.tsx
+++ src/SavedQuizDetails/SavedQuizDetails.test.tsx
@@ -8,3 +8,17 @@ import { SavedQuizGamesModel } from '../utils/utils';
 import mockSavedQuizData from '../SavedQuiz/mocksavedQuizData';
 
 const mockData: SavedQuizGamesModel = mockSavedQuizData;
+
+describe('SavedQuizDetails', () => {
+	test('should render all quiz information that is saved in full detail', () => {
+		render(
+			<MemoryRouter>
+				<SavedQuizDetails savedQuiz={mockData} />
+			</MemoryRouter>
+		);
+		expect(screen.getByText('Question: 1')).toBeInTheDocument();
+		expect(screen.getByText('Your Answer: Public')).toBeInTheDocument();
+		expect(screen.getByText('Question: 2')).toBeInTheDocument();
+		expect(screen.getByText('Your Answer: SG553')).toBeInTheDocument();
+	});
+});
