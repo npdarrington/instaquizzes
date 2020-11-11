d346e1eb89e9a8d2b73b58296e4b7acbfb31c054
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 17:32:27 -0700

    Wrapped SavedQuizzes render in MemoryRouter

--- src/SavedQuizzes/SavedQuizzes.test.tsx
+++ src/SavedQuizzes/SavedQuizzes.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
 
 import SavedQuizzes from './SavedQuizzes';
@@ -8,7 +9,11 @@ import mockSavedQuizzesData from './mockSavedQuizzesData';
 
 describe('SavedQuizzes', () => {
 	test('should display SavedQuizzes on the screen', () => {
-		render(<SavedQuizzes savedGames={mockSavedQuizzesData} />);
+		render(
+			<MemoryRouter>
+				<SavedQuizzes savedGames={mockSavedQuizzesData} />
+			</MemoryRouter>
+		);
 		expect(screen.getByText('Saved Quiz 1')).toBeInTheDocument();
 		expect(screen.getByText('Correct Answers: 1')).toBeInTheDocument();
 		expect(screen.getByText('Incorrect Answers: 1')).toBeInTheDocument();
@@ -18,7 +23,11 @@ describe('SavedQuizzes', () => {
 	});
 
 	test('should display a message to the user if no SavedQuizzes exist', () => {
-		render(<SavedQuizzes savedGames={[]} />);
+		render(
+			<MemoryRouter>
+				<SavedQuizzes savedGames={[]} />
+			</MemoryRouter>
+		);
 		expect(
 			screen.getByText(
 				'Play through a new InstaQuiz game and save it to be able to view it here'
