fd3dd1f091e2ecb6a08332146b54ace8e56d8e13
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 18:15:51 -0700

    Added additional imports to get render to work correctly

--- src/SavedQuiz/SavedQuiz.test.tsx
+++ src/SavedQuiz/SavedQuiz.test.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
 import SavedQuiz from './SavedQuiz';
+import { SavedQuizGamesModel } from '../utils/utils';
+import mockSavedQuizData from './mocksavedQuizData';
+
+const mockSavedData: any = mockSavedQuizData;
+
+describe('SavedQuiz', () => {
+	test('should render a SavedQuiz on the page', () => {
+		render(
+			<MemoryRouter>
+				<SavedQuiz savedQuiz={mockSavedData} />
+			</MemoryRouter>
+		);
+		screen.debug();
+	});
+});
