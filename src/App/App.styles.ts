b8e4974f60d9883757bfd11c69ca7591a4fbd69b
Nathan Darrington <npdarrington@icloud.com>
2020-11-13 16:28:42 -0700

    Impor styled and created Wrapper as styled div

--- src/App/App.styles.ts
+++ src/App/App.styles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import BGImage from '../images/tiffany-nguyen-bg.jpg';
 
 export const GlobalStyle = createGlobalStyle`
@@ -19,4 +19,31 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     font-family: 'Catamaran', sans-serif;
   }
+`;
+
+export const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	> p {
+		color: #fff;
+	}
+
+	h1 {
+		font-family: Fascinate Inline;
+		background-image: linear-gradient(180deg, #fff, #87f1ff);
+		background-size: 100%;
+		background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-color-fill: transparent;
+		-moz-background-clip: text;
+		-mox-text-fill-color: transparent;
+		filter: drop-shadow(2px 2px #0085a3);
+		font-size: 70px;
+		font-weight: 400;
+		text-align: center;
+		margin: 20px;
+		color: yellow;
+	}
 `;
