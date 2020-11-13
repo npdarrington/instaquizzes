8e29e8136c6c74fa9f467831da7f325203573b66
Nathan Darrington <npdarrington@icloud.com>
2020-11-13 16:10:42 -0700

    Created GlobalStyle styled component for App

---
+++ src/App/App.styles.ts
@@ -0,0 +1,19 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+    padding: 0 20x;
+    display: flex;
+    justify-content: center;
+  }
+
+  * {
+    box-sizing: border-box
+    font-family: 'Catamaran', sans-serif;
+  }
+`;
